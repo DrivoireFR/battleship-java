98fd45b640c684d3fe8e079841f717ab54dd37ec
Flori <florian.briere@gmail.com>
2025-11-20 15:10:45 +0100

    Chgts dans BoardRenderer pour grille dynamique

--- src/main/java/com/qualityworkshop/terminalbattleship/rendering/BoardRenderer.java
+++ src/main/java/com/qualityworkshop/terminalbattleship/rendering/BoardRenderer.java
@@ -10,13 +10,24 @@ public final class BoardRenderer {
 
     public static String render(Board board, boolean revealShips) {
         StringBuilder builder = new StringBuilder();
-        builder.append("  A B C D E F\n");
-        for (int row = 0; row < board.size(); row++) {
+        int size = board.size();
+
+        // En-tête des colonnes
+        builder.append("  ");
+        for (int col = 0; col < size; col++) {
+            builder.append((char)('A' + col));
+            if (col < size - 1) {
+                builder.append(' ');
+            }
+        }
+        builder.append('\n');
+
+        for (int row = 0; row < size; row++) {
             builder.append(row + 1).append(' ');
-            for (int col = 0; col < board.size(); col++) {
+            for (int col = 0; col < size; col++) {
                 CellState state = board.cellAt(new com.qualityworkshop.terminalbattleship.board.Coordinate(row, col));
                 builder.append(state.glyph(revealShips));
-                if (col < board.size() - 1) {
+                if (col < size - 1) {
                     builder.append(' ');
                 }
             }
@@ -25,3 +36,4 @@ public final class BoardRenderer {
         return builder.toString();
     }
 }
+
