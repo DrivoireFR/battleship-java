d70c24b8b75b185eff8900068989b53ed598474d
Flori <florian.briere@gmail.com>
2025-11-20 14:48:51 +0100

    Modifs GameConfiguration pour grille réduite

--- src/main/java/com/qualityworkshop/terminalbattleship/game/GameConfiguration.java
+++ src/main/java/com/qualityworkshop/terminalbattleship/game/GameConfiguration.java
@@ -10,8 +10,20 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class GameConfiguration {
 
-    private static final int BOARD_SIZE = 6;
-    private static final int SHIP_COUNT = 3;
+    private static final int BOARD_SIZE = getBoardSize();
+    private static final int SHIP_COUNT = getShipCount();
+
+    private static int getBoardSize() {
+        String miniBoard = System.getProperty("mini-board");
+        if ("true".equalsIgnoreCase(miniBoard)) {
+            return 4;
+        }
+        return 6;
+    }
+
+    private static int getShipCount() {
+        return BOARD_SIZE == 4 ? 2 : 3;
+    }
 
     @Bean(name = "playerBoard")
     public Board playerBoard() {
@@ -25,9 +37,9 @@ public class GameConfiguration {
 
     @Bean
     @Primary
-    public GameEngine gameEngine(@Qualifier("playerBoard") Board playerBoard, 
-                                  @Qualifier("computerBoard") Board computerBoard, 
-                                  ShotStrategy strategy) {
+    public GameEngine gameEngine(@Qualifier("playerBoard") Board playerBoard,
+                                 @Qualifier("computerBoard") Board computerBoard,
+                                 ShotStrategy strategy) {
         return new GameEngine(playerBoard, computerBoard, strategy);
     }
 }
