201cc8d172c0dfef7510ccf1d896b9e8722e1174
Albin R. <riviere.albin@gmail.com>
2025-11-20 12:51:39 +0100

    Ajout Fonction Affichage Coups

--- src/main/java/com/qualityworkshop/terminalbattleship/cli/GameRunner.java
+++ src/main/java/com/qualityworkshop/terminalbattleship/cli/GameRunner.java
@@ -3,6 +3,7 @@ package com.qualityworkshop.terminalbattleship.cli;
 import com.qualityworkshop.terminalbattleship.game.GameEngine;
 import com.qualityworkshop.terminalbattleship.game.ShotResult;
 import com.qualityworkshop.terminalbattleship.rendering.BoardRenderer;
+import com.qualityworkshop.terminalbattleship.history.HistoryTracker;
 import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
@@ -13,6 +14,10 @@ public class GameRunner {
     private final GameEngine gameEngine;
     private final Scanner scanner;
 
+    // Historique des coups
+    private final HistoryTracker playerHistory = new HistoryTracker(3);
+    private final HistoryTracker computerHistory = new HistoryTracker(3);
+
     public GameRunner(GameEngine gameEngine) {
         this.gameEngine = gameEngine;
         this.scanner = new Scanner(System.in);
@@ -22,14 +27,20 @@ public class GameRunner {
         System.out.println("Bienvenue dans Terminal Battleship ! Tapez 'quit' pour arrêter.\n");
         while (!gameEngine.isComputerFleetDestroyed() && !gameEngine.isPlayerFleetDestroyed()) {
             displayBoards();
+            displayHistory(); // afficher l'historique avant chaque tir
+
             ShotResult playerResult = askForPlayerShot();
             System.out.println(playerResult.message());
+            playerHistory.add(playerResult.coordinate() + " -> " + playerResult.outcome());
+
             if (playerResult.gameOver()) {
                 break;
             }
 
             ShotResult computerResult = gameEngine.computerShoots();
             System.out.println("\nOrdinateur: " + computerResult.message());
+            computerHistory.add(computerResult.coordinate() + " -> " + computerResult.outcome());
+
             if (computerResult.gameOver()) {
                 break;
             }
@@ -42,6 +53,14 @@ public class GameRunner {
         System.out.println(BoardRenderer.render(gameEngine.playerBoard(), true));
         System.out.println("\nGrille adverse (brouillard)");
         System.out.println(BoardRenderer.render(gameEngine.computerBoard(), false));
+    }
+
+    private void displayHistory() {
+        System.out.println("\nHistorique des 3 derniers coups :");
+        System.out.println("Joueur :");
+        playerHistory.getHistory().forEach(entry -> System.out.println("  " + entry));
+        System.out.println("Ordinateur :");
+        computerHistory.getHistory().forEach(entry -> System.out.println("  " + entry));
     }
 
     private ShotResult askForPlayerShot() {
